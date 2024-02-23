6b7f1acd48615ab5c37594621574bdf50fdff923
irohanrajput <itusharsingh27@gmail.com>
2024-02-24 05:13:09 +0530

    added products

---
+++ products/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
