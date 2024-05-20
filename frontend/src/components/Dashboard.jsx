287040313f0dd4f58cecd953845bc0a9c2944a48
irohanrajput <itusharsingh27@gmail.com>
2024-05-20 06:52:49 +0530

    new files

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Hello! Welcome to the Dashboard</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">Ecom</Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
